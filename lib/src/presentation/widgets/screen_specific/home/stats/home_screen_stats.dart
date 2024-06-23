bc7d1c7382928bbc81da5d015a1adbbabbfb8d3b
Muhammad Usama Ijaz <muhammadusamaijaz7@gmail.com>
2024-06-23 21:26:00 +0500

    home screen animation duration fixes

--- lib/src/presentation/widgets/screen_specific/home/stats/home_screen_stats.dart
+++ lib/src/presentation/widgets/screen_specific/home/stats/home_screen_stats.dart
@@ -24,7 +24,7 @@ class HomeScreenStats extends StatelessWidget {
                       height: 0.42.sw,
                       child: Center(
                         child: AnimatedContainer(
-                          duration: const Duration(milliseconds: 500),
+                          duration: const Duration(seconds: 1),
                           width: statsAnimationStarted ? 0.42.sw : 0,
                           height: statsAnimationStarted ? 0.42.sw : 0,
                           decoration: BoxDecoration(
@@ -34,7 +34,7 @@ class HomeScreenStats extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               AnimatedSize(
-                                duration: const Duration(milliseconds: 500),
+                                duration: const Duration(seconds: 1),
                                 child: SizedBox(
                                     height: statsAnimationStarted ? 15.h : 0),
                               ),
@@ -72,7 +72,7 @@ class HomeScreenStats extends StatelessWidget {
                                   child: const Text("offers",
                                       overflow: TextOverflow.fade)),
                               AnimatedSize(
-                                duration: const Duration(milliseconds: 500),
+                                duration: const Duration(seconds: 1),
                                 child: SizedBox(
                                     height: statsAnimationStarted ? 15.h : 0),
                               ),
@@ -86,7 +86,7 @@ class HomeScreenStats extends StatelessWidget {
                       height: 0.42.sw,
                       child: Center(
                         child: AnimatedContainer(
-                          duration: const Duration(milliseconds: 500),
+                          duration: const Duration(seconds: 1),
                           width: statsAnimationStarted ? 0.42.sw : 0,
                           height: statsAnimationStarted ? 0.42.sw : 0,
                           decoration: BoxDecoration(
@@ -96,7 +96,7 @@ class HomeScreenStats extends StatelessWidget {
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               AnimatedSize(
-                                duration: const Duration(milliseconds: 500),
+                                duration: const Duration(seconds: 1),
                                 child: SizedBox(
                                     height: statsAnimationStarted ? 15.h : 0),
                               ),
@@ -137,7 +137,7 @@ class HomeScreenStats extends StatelessWidget {
                                   child: const Text("offers",
                                       overflow: TextOverflow.fade)),
                               AnimatedSize(
-                                duration: const Duration(milliseconds: 500),
+                                duration: const Duration(seconds: 1),
                                 child: SizedBox(
                                     height: statsAnimationStarted ? 15.h : 0),
                               ),
